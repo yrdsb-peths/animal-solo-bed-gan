87fd14bce12b2527c73e23da017786e958d19ea4
bed-gan <440021905@gapps.yrdsb.ca>
2025-05-01 16:32:55 -0400

    Added words to title screen

--- TitleScreen.java
+++ TitleScreen.java
@@ -8,7 +8,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class TitleScreen extends World
 {
-    Label titleLabel = new Label("Hungry Elephant", 60);
+    Label titleLabel = new Label("The Elephant", 60);
 
     /**
      * Constructor for objects of class TitleScreen.
@@ -18,10 +18,11 @@ public class TitleScreen extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1); 
-        
-        addObject(titleLabel, getWidth()/2, getHeight()/2);
+
+        addObject(titleLabel, 225, 100);
+        prepare();
     }
-    
+
     /**
      * The main world act loop
      */
@@ -31,5 +32,20 @@ public class TitleScreen extends World
             MyWorld gameWorld = new MyWorld();
             Greenfoot.setWorld(gameWorld);
         }
+    }
+    
+    /**
+     * Prepare the world for the start of the program.
+     * That is: create the initial objects and add them to the world.
+     */
+    private void prepare()
+    {
+        Elephant elephant = new Elephant();
+        addObject(elephant,490,100);
+        Label label = new Label("Use <- and -> to move", 50);
+        addObject(label,300,200);
+        Label label2 = new Label("Press <space> to start", 50);
+        addObject(label2,300,290);
+        elephant.setLocation(461,101);
     }
 }
